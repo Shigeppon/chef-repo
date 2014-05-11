f848ef5105dfd8d9f0a42eaedc0359081f2f8a7a
Shigeo Sakamoto <shigeo.sakamoto@gmail.com>
2014-05-12 01:05:06 +0900

    install git-1.8.4

---
+++ site-cookbooks/devel-pkg/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: devel-pkg
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "zlib-devel" do
+  action :install
+end
