ad5b217a05c027214d4a6ec60fc8edf10314bf3b
Shigeo Sakamoto <shigeo.sakamoto@gmail.com>
2014-05-12 17:44:01 +0900

    バグ対応

--- site-cookbooks/base/recipes/default.rb
+++ site-cookbooks/base/recipes/default.rb
@@ -10,6 +10,7 @@
 file '/etc/yum.conf' do 
   _file = Chef::Util::FileEdit.new(path)
   _file.search_file_replace_line('exclude=kernel', "#exclude=kernel\n")
-  content _file.send(:contents).join
+  _file.write_file
+  #content _file.send(:contents).join
   action :create
 end.run_action(:create)
